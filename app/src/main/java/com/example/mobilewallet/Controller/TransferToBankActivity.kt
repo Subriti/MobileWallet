a73827fe9f5604b3786eb7e3322d3d983b600d6d
Subriti <subritiaryal13@gmail.com>
2022-09-05 22:00:33 +0545

    Built Send to Bank UI

--- app/src/main/java/com/example/mobilewallet/Controller/TransferToBankActivity.kt
+++ app/src/main/java/com/example/mobilewallet/Controller/TransferToBankActivity.kt
@@ -1,12 +1,19 @@
 package com.example.mobilewallet.Controller
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import com.example.mobilewallet.R
 
 class TransferToBankActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_transfer_to_bank)
+
+        val loadback= findViewById<ImageView>(R.id.loadMoney)
+        loadback.setOnClickListener {
+            startActivity(Intent(this@TransferToBankActivity,SendMoneyActivity::class.java))
+        }
     }
 }
